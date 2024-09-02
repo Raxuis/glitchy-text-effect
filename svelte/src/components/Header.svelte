a3e633dbef53e669e3248d76271a2be5422fed47
Raxuis <raclot.raphael@gmail.com>
2024-09-02 17:52:16 +0200

    feat(Svelte): :sparkles: implemented svelte

---
+++ svelte/src/components/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+    const inputChange = (e) => {
+        console.log(e.target.value);
+    };
+</script>
+
+<header>
+    <h1 class="title">Glitch text effect !</h1>
+    <p>Hover over the text to see the effect</p>
+
+    <h2 class="title">Settings</h2>
+    <div class="settings">
+        <label for="glitch-frequency">Glitch frequency</label>
+        <input
+            type="range"
+            id="glitch-frequency"
+            min="0"
+            max="1"
+            step="0.01"
+            value="0.7"
+            on:input={(e) => inputChange(e)}
+        />
+        <output>0.7</output>
+    </div>
+</header>
